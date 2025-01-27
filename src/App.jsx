0cad263a593acafb1805e8732b752ccbe0f5fa56
Amit Dhiman <amitdhiman5086@gmail.com>
2025-01-27 11:23:53 +0530

    Add Footer and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
 
-
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <h1>Hii</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+function AppLayout() {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <Navbar />
+
+      <div className="min-h-[70vh]">
+        <Outlet />
+      </div>
+
+      <Footer />
+    </div>
   );
 }
 
