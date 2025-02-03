63dd12f78d8295aa543eb771ea28b512fb97f137
Amit Dhiman <amitdhiman5086@gmail.com>
2025-02-03 09:06:04 +0530

    Final

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "https://d-match.onrender.com/api";
-export const BASE_URL = "http://13.60.18.36:3000/api/";
+export const BASE_URL = "http://13.60.18.36:3000/api";
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://13.60.18.36:3000",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, '')
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://13.60.18.36:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
 });
