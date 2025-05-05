2d3beeaf823347944db7474e681ea41bae8c6101
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-05-05 17:38:25 +0530

    fix: update API proxy target to use BASE_URL for better configuration management

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { BASE_URL } from "./src/Utils/constant";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://d-match-backend.onrender.com",
+        target: BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
